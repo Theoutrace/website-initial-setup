9c65f0db65a7660bfa70aaf71359addcdab6a1c7
Theoutrace <prakashkumar.imw@gmail.com>
2025-05-28 18:35:44 +0530

    --

--- src/utils/store/slices/pageSlice.js
+++ src/utils/store/slices/pageSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   pageLoading: false,
-  pageLoaderText: '',
+  pageLoaderText: 'Loading...',
   modalLoading: false,
   authLoading: false
 };
